6097891728636106a168e35e1c5f77935fa103c0
SergioCalde <sergio.calderon.dm@outlook.com>
2022-09-09 23:51:22 -0600

    Agregar Middlewares para validar campos en API

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,17 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 
 const crearUsuario = (req, res = response) => {
+
+    const errors = validationResult( req );
+    if( !errors.isEmpty() ) {
+        return res.status(400).json(
+            {
+                ok: false,
+                errors: errors.mapped()
+            });
+    } 
 
     const { email, name, password } = req.body
     console.log(email, name, password );
@@ -14,6 +24,16 @@ const crearUsuario = (req, res = response) => {
 }
 
 const loginUsuario = (req, res) => {
+
+    const errors = validationResult( req );
+
+    if( !errors.isEmpty() ) {
+        return res.status(400).json(
+            {
+                ok: false,
+                errors: errors.mapped()
+            });
+    }
 
     const { email, password } = req.body
     console.log(email, password);
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,25 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
 const router = Router();
 
 // Crear un nuevo usuario
-router.post( '/new', crearUsuario );
+router.post( '/new',
+[
+    check('email', 'El email es obligatorio').isEmail(),
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    check('password', 'El password no cumple los requisitos').isLength({ min: 6 }),
+    
+],crearUsuario );
 
 // Login de usuario
-router.post( '/', loginUsuario);
+router.post( '/', [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    check('password', 'El password no cumple los requisitos').isLength({ min: 6 }),
+] ,loginUsuario );
 
 // Validar y revalidar token
 router.get( '/renew', revalidarToken);
