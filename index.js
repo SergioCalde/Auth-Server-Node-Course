f2e610b9857e05f053d5e37127ab1be66cc5524e
SergioCalde <sergio.calderon.dm@outlook.com>
2022-09-15 09:09:36 -0600

    add front-end to aplication, and get all paths.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -38,6 +39,11 @@ app.use( express.json() );
 
 // Rutas 
 app.use( '/api/auth', require( './routes/auth' ) );
+
+// Manejar todas las demás rutas
+app.get( '*', ( req, res ) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) )
+} )
 
 
 app.listen( process.env.PORT, () => {
